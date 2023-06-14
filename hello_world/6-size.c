8ebe2c4350b987318acb89b31df6cdcd23d25b04
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-14 13:33:02 -0500

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 /*
  * File: 6-size.c
- * Auth: David Vasquez
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
@@ -11,13 +11,13 @@
  *
  * Return: Always 0.
  */
-int main(void) 
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %zu byte(s)", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)", sizeof(float));
-	
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 	return (0);
 }
