1a85421b8f2b4f856f5a9a646dfcec6d50f80f6b
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-20 09:05:45 -0500

    act001

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
 /*
  * File: 0-positive_or_negative.c
  * Auth: David Vasquez
  */
 
-/* betty style doc for function main goes there */
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - will assign a random number to th
- *        e variable n each time it is executed.
+ * main - will assign a random number to the
+ *        variable n each time it is executed.
  *
  *        Return: Always 0.
  */
@@ -25,7 +22,7 @@ int main(void)
 		printf(%d "is positive\n", n);
 	else if (n < 0) 
 		printf(%d "is negative", n);
-	else if (n == 0)
+	else (n == 0)
 		printf(%d "is zero", n);
 	
 	return (0)
