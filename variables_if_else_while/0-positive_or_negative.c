8cbdc321269c13375b4cc17d2758be3fdf8d07ea
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-20 09:14:21 -0500

    act001

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -22,9 +22,9 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0) 
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	
 	return (0);
 }
