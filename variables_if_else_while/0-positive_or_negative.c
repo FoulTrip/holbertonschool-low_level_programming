bd93ec59d491ecbec10c0878792d0f20ffe2744a
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-20 09:29:51 -0500

    act001

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,9 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 		printf("%d is positive\n", n);
@@ -25,6 +26,6 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	
+
 	return (0);
 }
