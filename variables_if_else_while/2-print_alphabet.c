5d6c588756e2cff23c04435120860e949d483f65
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-20 14:31:35 -0500

    act003

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 /*
  * File: 2-print_alphabet.c
- * Auth: David Vasquez
+ * Auth: David vasquez M
  */
 
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
@@ -14,7 +14,7 @@ int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++);
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
