8034e6bcb9662f0d494a4b5ad84c213854e44d62
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-21 07:47:04 -0700

    act004

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: David Vasquez 
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowcase
+ *        and then in uppercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar("\n");
+}
