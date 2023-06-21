274e3a2f7c36aeab03e33082f1efe1265e7b221d
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-21 14:54:21 -0500

    act003

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,10 @@
-#include "main.h"
-#include <unistd.h>
-void print_alphabet_x10(void);
+#include "holberton.h"
 
 /**
  * main - prints 10 times the alphabet
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    void print_alphabet_x10();
-    return (0);
-}
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 void print_alphabet_x10(void)
 {
 	char c;
