74624efbddf818eea2b7bdaea5374204f783461b
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-21 08:20:32 -0700

    act006

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: David Vasquez
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  prints all single digit numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	for (int number = 0; number < 10; number++)
+		printf(number)
+
+	return (0);
+}
