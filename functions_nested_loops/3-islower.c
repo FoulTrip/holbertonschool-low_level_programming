fa320ce7276e2703f944e067ea7eb179ae1d57f9
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-21 15:18:56 -0500

    act005

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - 
+ * _islower - Checks for lowercase character
  *
  * lowercase in ASCII: 97 - 122
  *
@@ -9,7 +9,7 @@
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122) 
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
