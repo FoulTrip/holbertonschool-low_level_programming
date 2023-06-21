5f3f732e2470765c38adc08296a9d3f2088774a1
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-21 13:47:37 -0700

    act007

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ *
+ * Return: 0 and 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		putchar('0');
+		return (-1);
+	}
+
+	return (0);
+}
