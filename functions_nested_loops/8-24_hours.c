ca5c32e2dcdfb3067b3682e2d15e17dcf326586e
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-21 18:51:50 -0700

    act009

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+/**
+ * jack_bauer -  prints every minute of the day
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int hrs;
+	int mnts;
+
+	for (mnts = 00; mnts < 60; mnts++)
+	{
+		if (mnts == 60)
+		{
+		       for (hrs == 00;hrs < 23; hrs++)
+		       {
+			       printf("%d:%d", hrs, mnts)
+		       }	       
+		}
+	}
+}
