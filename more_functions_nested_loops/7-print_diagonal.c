a3c848a0c48dc7dfa2b53f0b8d4cafafc0ca2334
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-23 11:00:30 -0500

    act007

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,35 +3,30 @@
  * Auth: David Vasquez
  */
 
-#include "main.h"
+#include main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal.
- * @n: paremeter
- *
- * Return: Always 0.
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: parameter
  */
 void print_diagonal(int n)
 {
-	int diagonal, space
+	int diagonal, space;
 
 	if (n > 0)
 	{
 		for (diagonal = 0; diagonal < n; diagonal++)
-	        {
+		{
 			for (space = 0; space < diagonal; space++)
-		        {
 				_putchar(' ');
-		        }
 			_putchar('\\');
 
 			if (diagonal == n - 1)
-		        {
 				continue;
-		        }
-		}
 
-		_putchar('\n');
+			_putchar('\n');
+		}
 	}
-	
+
+	_putchar('\n');
 }
