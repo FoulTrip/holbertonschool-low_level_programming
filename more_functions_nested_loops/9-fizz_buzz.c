9d46f3abfbc020cee116ea0dfbcbef2ae57308c0
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-23 09:22:23 -0700

    act008

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+/*
+ * File: 9-fizz_buzz.c
+ * Auth: David Vasquez 
+ */
+
+#include "main.h"
+
+/**
+ * main - function de fizz buzz
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int initNum;
+
+	for (initNum = 0; initNum <= 100; initNum++)
+	{
+		if (initNum % 3 == 0)
+		{
+			printf("fizz"); 
+		}
+
+
+		if (initNum % 5 == 0)
+		{
+			printf("buzz");
+		}
+
+		if (initNum % 3 == 0 && initNum % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+	}
+
+	return (0);
+}
