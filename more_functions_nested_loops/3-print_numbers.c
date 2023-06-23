02ae3d1a865e46aaa7813a8fa30a8f62487f38d5
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-23 09:51:04 -0500

    act003

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -10,11 +10,9 @@
  * 
  * Return: Always 0.
  */
-void print_numbers(void)
-{
-	int num;
-	
-	for (int num = 0; num <= 9; num++)
+void print_numbers(int num)
+{	
+	for (num = 0; num <= 9; num++)
 		_putchar((num % 10) + '0');
 
 	_putchar('\n');
