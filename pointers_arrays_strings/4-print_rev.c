a622be71590e7e7632d6fa7b6d49165542871f1a
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-27 11:35:50 -0500

    act006

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,21 +6,18 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string.
+ * print_rev - Reverses a string.
  * @s: The string to be reversed.
  */
 void print_rev(char *s)
 {
-	int len = 0, index = 0;
-	char tmp;
+	int len = 0, index;
 
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= len / 2; index--)
-	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
-	}
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
 }
