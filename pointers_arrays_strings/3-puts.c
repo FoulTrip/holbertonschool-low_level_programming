8287723916afbbae33a9d8c39f94f8598c4c6064
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-27 08:55:59 -0700

    act004

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-puts.c
+ * Auth: David Vasquez
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: Pointer parameter
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	printf("%s", *str);
+}
