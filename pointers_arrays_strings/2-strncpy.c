babad71cca1a784f8b169e611c65228c2dd5b2ca
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-28 10:30:34 -0500

    act015

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index;
 
-	for (index = o; index < n && src[i] != '\0'; index++)
+	for (index = 0; index < n && src[index] != '\0'; index++)
 		dest[index] = src[index];
 
-	for (; index < n; index++])
+	for (; index < n; index++)
 		dest[index] = '\0';
 
 	return dest;
