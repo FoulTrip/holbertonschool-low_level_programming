86a59a2a614a5a3bde74b0b3f88df3da5af59e74
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-06-28 10:59:14 -0500

    act016

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,7 @@ void reverse_array(int *a, int n)
 	int left = 0;
 	int right = n - 1;
 
-	while (left < right)
-	{
+	while (left < right) {
 		int temp = a[left];
 		a[left] = a[right];
 		a[right] = temp;
