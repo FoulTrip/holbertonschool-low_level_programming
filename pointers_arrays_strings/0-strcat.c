d7a51349e3d304bbfde40b9c8860efa3a05d42ad
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-28 07:53:36 -0700

    act013

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
-  * *_strcat -  concatenates two strings.
-  * @*dest - chain text of destine
-  * @*src - font of text
-  */
+ * _strcat - concatenates two strings
+ * @dest: This is the output dest
+ * @src: This is the input source
+ * 
+ * Return: String concatenated
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *stringFinal = dest;
