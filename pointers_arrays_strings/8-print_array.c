f4c9382007fad8580be57df86a7cf93c743dfe46
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-27 20:06:56 -0700

    act011

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-print_array.c
+ * Auth: David Vasquez 
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array -  prints n elements of an array of integers
+ * @a: pointer
+ * @n: parameter
+ *
+ * Return: n is the number of elements of the array to be printed
+ */
+void print_array(int *a, int n)
+{
+	int count;
+
+	for (count = 0; count < n; count++)
+	{
+		printf("%d", *a[count]);
+		if (count != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
