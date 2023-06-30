7c3e070d0009a465ea88721d7a24c7ddb64dc57d
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-30 08:14:17 -0700

    act026

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: original destination
+ * @src: area memory
+ *
+ * Return: copy Memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char copyMemory = dest;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--
+	}
+
+	return (copyMemory);
+}
