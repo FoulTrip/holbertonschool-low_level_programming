ba1f934061391daf8be7c459f267ee8de1cfa9a7
FoulTrip <davidvasquezmahecha@gmail.com>
2023-06-30 09:05:36 -0700

    act029

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: Init String
+ * @accept: find in String
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index, count;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		for (count = 0; accept[count] != '\0'; count++)
+		{
+			if (accept[count] == s[index])
+			{
+				return (&s[index]);
+			}
+		}
+	}
+
+	return (NULL);
+}
