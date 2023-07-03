20796e42325a36babae6d4ab877153c101662fcd
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-03 11:28:47 -0500

    act002

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,8 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
+	// Verificamos si "*s" no apunta al carácter nulo
 	if (*s)
 	{
+		// Esto desplaza el puntero s al siguiente carácter en la cadena.
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
