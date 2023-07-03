812e5e7f8af76831979a3045a45f3171271fcbfd
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-03 09:00:01 -0700

    act001

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: parameter string
+ *
+ * Return: print
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+
+	return (0);
+}
---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+Excersice about recursion in C
