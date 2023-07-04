a0914b15d037855cfc5fc37fc1d5abccd87b5df7
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-04 10:38:37 -0500

    act006

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,6 +10,10 @@
  */
 int copy_sqrt_recursion(int a, int b)
 {
+	/* we take two parameters: a and b. We compare if "a" 
+	   is equal to the square of "b", if so, we return "b", 
+           which means that "b" is the square root of "a". 
+	   If the square of b is greater than or equal to "a", we return -1.*/
 	if (a == (b * b))
 		return (b);
 	else if ((b * b) >= a)
@@ -26,6 +30,10 @@ int copy_sqrt_recursion(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
+	/* The function "_sqrt_recursion" takes a number "n" as a parameter. 
+            If "n" is less than or equal to 0, it returns -1. Otherwise, it calls 
+	    "copy_sqrt_recursion" with the values "n" and 0, which starts the 
+            recursive process to find the square root of "n". */
 	if (n <= 0)
 		return (-1);
 	return (copy_sqrt_recursion(n, 0));
