4f84d239a5da6cd17331054c6c3c2b94885a4320
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-04 07:21:41 -0700

    act03

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - length of a string.
+ * @s: parameter string
+ *
+ * Return: lenght
+ */
+int _strlen_recursion(char *s)
+{
+	int lenght = strlen(*s);
+
+	printf("%d\n", lenght);
+
+	return (0);
+}
