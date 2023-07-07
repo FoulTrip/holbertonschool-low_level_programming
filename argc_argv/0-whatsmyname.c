3ece387dabd1a6d47f0167aabbb56411b0a255db
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-07 11:14:57 -0500

    act003

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -11,13 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	/* argv[0]: Esta línea imprime el primer elemento de argv,
-	 * que es la cadena de caracteres que representa el nombre del programa. */
 	printf("%s\n", argv[0]);
-	/* Esta línea está utilizando la sintaxis (void) para evitar  la advertencia
-	 * de "variable no utilizada" que podría surgir si no se está utilizando argc 
-	 * en el programa. Simplemente se está indicando al compilador que ignore la 
-	 * variable argc en este caso. */
 	(void)argc;
 	return (0);
 }
