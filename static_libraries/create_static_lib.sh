7d7719c444af109c12b39da1e85a0d2964070a7b
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-07 07:50:55 -0700

    act004

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-C_Files=$(find . -maxdepth 1 -type f -name "*.c")
-
-for file in $C_Files; do
-	gcc -c "$file"
-done
-
-ar rcs libmy.a *.o
-
-rm -f *.o
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a
