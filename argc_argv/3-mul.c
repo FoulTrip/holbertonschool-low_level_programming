9f811224dbbf9a063ad3ca866eae8d3eda563bc5
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-07 09:41:21 -0700

    act005

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -  multiplies two numbers.
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplicando;
+
+	multiplicando = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (index = 1; index < argc; index++)
+	{
+		multiplicando = multiplicando * atoi(argv[index]);
+	}
+
+	printf("%d\n", multiplicando);
+	return (0);
+}
