aafb73f202a1f027a42a907149c10c720f280eba
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-07 11:27:33 -0500

    act004

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,6 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
+	/* 
+           Al restar 1, se obtiene el número de argumentos de línea de comandos
+           pasados al programa excluyendo el nombre del programa en sí.
+	*/
 	printf("%d\n", argc - 1);
 	(void)argv;
 	return (0);
