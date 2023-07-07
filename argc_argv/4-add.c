cc778a29750f2bdb4b4c0f59b7fa5fa62d534912
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-07 09:52:42 -0700

    act006

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, index;
+
+	if (argc > 1)
+	{
+		for (index = 1; index < argc; index++)
+		{
+			int b;
+			char *str;
+
+			str = argv[index];
+			for (b = 0; str[b] != '\0'; b++)
+			{
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return(1);
+				}
+			}
+		}
+	}
+	for (index = 1; index < argv; index++)
+	{
+		sum += atoi(argv[index]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
