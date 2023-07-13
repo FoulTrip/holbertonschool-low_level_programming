ecc3eaaae417b39b0d25d0b0e98e44db4f03e2e6
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-13 07:01:20 -0700

    act003

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: concatenated String
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int firstIndex, secondIndex;
+	char *finalString;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	finalString = (char *) malloc(((firstIndex + secondIndex) + 1) * sizeof(char));
+
+	if (finalString == NULL)
+		return (NULL);
+
+	for (firstIndex = 0; s1[firstIndex] != '\0'; firstIndex++)
+	{
+		finalString[firstIndex] = s1[firstIndex];
+	}
+	for (secondIndex = 0; s2[secondIndex] != '\0'; secondIndex++)
+	{
+		finalString[firstIndex] = s2[secondIndex];
+		firstIndex++;
+	}
+
+	return (finalString)
+		
+		return (finalString);
+}
