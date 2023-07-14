1eb62f719d5514e7c84ed0c03db910b326f12620
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-14 07:08:10 -0700

    act002

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: limit
+ * 
+ * Return: string concatened
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int firstIndex, secondIndex;
+	char *newString;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (firstIndex = 0; s1[firstIndex] != '\0'; firstIndex++)
+	{
+	}
+
+	for (secondIndex = 0; s2[secondIndex] != '\0'; secondIndex++)
+	{
+	}
+
+	if (n < b)
+		n = b;
+
+	newString = malloc(((a + n) + 1));
+
+	if (newString == NULL)
+		return (NULL);
+
+	for (firstIndex = 0; s1[firstIndex] != '\0'; firstIndex++)
+	{
+		newString[firstIndex] = s1[firstIndex];
+	}
+
+	for (secondIndex = 0; secondIndex != n; secondIndex++)
+	{
+		newString[firstIndex] = s2[secondIndex];
+		firstIndex++;
+	}
+
+	newString[firstIndex] = '\0';
+	return (newString);
+}
