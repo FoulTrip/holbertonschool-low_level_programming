fb950adbe8c7d607771029ba74e75e018f6826a1
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-14 09:36:28 -0500

    act003

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
         char *content;
         unsigned int index;
 
-	if (memory == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	content = memory;
