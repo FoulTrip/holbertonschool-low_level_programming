7f2aa5d9fc5f5336e6181c624e08ff237464747f
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-18 09:47:25 -0500

    act001

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
 };
 typedef struct dog dog;
 dog *new_dog(char *name, float age, char *owner);
-void init dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 #endif
