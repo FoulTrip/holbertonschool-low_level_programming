b5b1dff87f1145e3cd137e6ca32676e54b28565c
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-18 07:43:49 -0700

    act001

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,8 @@
+#ifndef DOH_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct dog - Description of the dog
  * @name: Name
@@ -11,3 +16,9 @@ struct dog
 	float *age;
 	char *owner;
 }
+typedef struct dog dog
+dog *new_dog(char *name, float age, char *owner);
+void init dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+#endif
