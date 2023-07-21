0b3b1b5ec59e0ec68ad9b8dcadd77eed84c8c873
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-21 08:47:03 -0500

    act002

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * init_dog - initialize a variable
+ * @d: pointer to the adress to initialize.
+ * @name: The dog's name'.
+ * @age: The dog's age'.
+ * @owner: The dog owner's name.
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
