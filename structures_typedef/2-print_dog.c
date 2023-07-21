baed4a3614f5062d3e6e5b8c74368dabadd87400
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-21 06:55:03 -0700

    act002

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,1 +1,29 @@
-void print_dog(struct dog *d);
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nill)\n");
+	else
+		printf("Name: %s\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nill)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nill)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
