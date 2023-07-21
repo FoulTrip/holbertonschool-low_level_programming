6e7bf6935de0f40d56dc65ad878789a7cb95ba22
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-21 17:32:58 -0500

    act004

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,7 +24,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", s);
 		if (separator != NULL && (count + 1) != n)
+		{
 			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(ap);
