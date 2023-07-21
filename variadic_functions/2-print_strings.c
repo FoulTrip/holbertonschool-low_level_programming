ee5ce31dd5daa84f6a476aac5655262a3d8d1b2e
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-07-21 18:42:19 -0500

    act004

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,15 +3,9 @@
 #include "variadic_functions.h"
 
 /**
-<<<<<<< HEAD
  * print_strings - prints strings
  * @separator: Input separator
  * @n: Number item
-=======
- * print_strings -  function that prints strings, followed by a new line
- * @separator: This is the input separator
- * @n: This is the number of items
->>>>>>> a9d206f45a78a3c493db1883a4ae1c2dd9024431
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
