8b747ba2a503eff5fc3fde895a7f74083a77d9c9
FoulTrip <davidvasquezmahecha@gmail.com>
2023-08-02 08:08:33 -0700

    act002

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - returns the number of elements in a linked list.
+ * @h: value
+ *
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
