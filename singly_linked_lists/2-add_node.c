1a79b8306bc2bae181ba0de137f0a83e1aa1eb7a
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-03 10:26:15 -0500

    act003

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -34,7 +34,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = len(str);
+	new_node->len = mblen(str);
 	new_node->next = (*head);
 	(*head) = new_node;
 
