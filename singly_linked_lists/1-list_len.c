298e92ba052cb2789ae00de56e8b51dc85af2a85
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-03 09:54:24 -0500

    act002

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,23 +1,21 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list.
- * @h: value
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- * Return: number of elements
+ * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
-	int count = 0;
+	size_t elements = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
-		count += 1;
+		elements++;
 		h = h->next;
 	}
 
-	return (count);
+	return (elements);
 }
