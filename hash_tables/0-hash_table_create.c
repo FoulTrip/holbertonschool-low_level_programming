43ef8dec8955aa0574c8f58ebb27f6c1d0932255
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-11 09:45:33 -0500

    act001

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 		new_hash_table->array = calloc(size, sizeof(hash_node_t *));
 
-		if (new_table_hash->array == NULL)
+		if (new_hash_table->array == NULL)
 		{
 			free(new_hash_table);
 			return (NULL);
