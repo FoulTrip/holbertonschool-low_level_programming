d984b4e5f040754170e39baaec8e4ae95cbd6c24
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-11 10:36:17 -0500

    act004

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,13 +31,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
-	new = malloc(sizeof(hash_node_t));
+	new_hash_table = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
 		free(value_copy);
 		return (0);
 	}
-	new->key = strdup(key);
+	new_hash_table->key = strdup(key);
 	if (new->key == NULL)
 	{
 		free(new);
