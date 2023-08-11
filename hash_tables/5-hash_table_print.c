8313f8a1eb3247aa25378c13481996fbcc70440d
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-11 11:13:39 -0500

    act006

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (subIndex = 0; subIndex < ht->size; subIndex++)
 	{
-		if (ht-array[subIndex])
+		if (ht->array[subIndex])
 		{
 			pointer = ht->array[subIndex];
 			while (pointer)
