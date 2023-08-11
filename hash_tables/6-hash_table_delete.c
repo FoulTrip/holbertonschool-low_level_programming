d90f9975407c80b539c0b17bf8231c36e93ef36e
FoulTrip <davidvasquezmahecha@gmail.com>
2023-08-11 09:23:32 -0700

    act007

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: is the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int subIndex;
+	hash_table_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (subIndex = 0; subIndex < ht->size; subIndex++)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[subIndex])
+			{
+				temp = ht->array[subIndex];
+
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				ht->array[subIndex] = ht->array[i]->next;
+			}
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
