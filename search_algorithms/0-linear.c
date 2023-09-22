433a0c44e64855de51aad44d4f106c51f5473aae
foultrip <davidvasquezmahecha@gmail.com>
2023-09-22 08:50:59 -0500

    act001

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Busca un valor en un array de enteros utilizando la búsqueda lineal.
+ * 
+ * @array: Puntero al primer elemento del array.
+ * @size: Tamaño del array.
+ * @value: Valor que se busca.
+ * 
+ * Return: El índice donde se encontró el valor o -1 si no se encontró.
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
+		if (array[i] == value)
+			return ((int)i);
+	}
+	
+	return (-1);
+}
